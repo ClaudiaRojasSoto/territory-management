cf900589ea1ce15267284676a4aba86e63f0b31e
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2025-11-01 19:03:28 -0300

    Fix: Comment out Devise configuration in User model
    - Devise is not installed/configured in the project yet
    - Caused NoMethodError when creating territories with assigned_to
    - Commented out devise lines with TODO note
    - Will be re-enabled when Devise is properly set up
    - Allows territory creation to work without authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # TODO: Uncomment when Devise is installed and configured
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
          
   # Relaciones
   has_many :assigned_territories, class_name: 'Territory', foreign_key: 'assigned_to_id'
